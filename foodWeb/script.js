69e4fe966631e8543e8188fe84c5ed6cf5fe7b19
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2023-01-22 17:43:52 +0100

    first commit

---
+++ foodWeb/script.js
@@ -0,0 +1,23 @@
+//this is the slidedown for the navigation bar.
+document.querySelector('.menu').addEventListener('click', ()=>{
+    document.querySelectorAll('.target').forEach((item)=>{
+        item.classList.toggle('slidedown');
+    })    
+}) 
+
+//Slideshow effect
+const icons = document.querySelectorAll('.section-1-icons i');
+let counter = 1;
+// SetInterval is a built-in function in JavaScript to 
+setInterval(() =>{
+    counter++;
+    const icon = document.querySelector('.section-1-icons .change');
+    icon.classList.remove('change');
+    
+    if(counter > icons.length){
+        icons[0].classList.add('change');
+        counter = 1;
+    }else{
+        icon.nextElementSibling.classList.add('change');
+    }
+}, 2000);
